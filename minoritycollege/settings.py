611ead5cbea36ec98e0d0d90bf1fef10cc2790d4
Salmanul Faris <salmanul.faris@shamalgroup.ae>
2026-03-31 12:15:54 +0400

    updated form

--- minoritycollege/settings.py
+++ minoritycollege/settings.py
@@ -135,7 +135,7 @@ STORAGES = {
         'BACKEND': 'django.core.files.storage.FileSystemStorage',
     },
     'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+        'BACKEND': 'minoritycollege.storage.NonStrictCompressedManifestStaticFilesStorage',
     },
 }
 
